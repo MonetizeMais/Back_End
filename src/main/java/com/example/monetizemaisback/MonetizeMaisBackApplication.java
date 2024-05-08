88b11ed9f97524432978934764a170b58e3d2334
Nicolas Granatelli Sanches <nicolassanches-ieg@instituto.ad>
2024-05-08 08:36:11 -0300

    subindo a api

---
+++ src/main/java/com/example/monetizemaisback/MonetizeMaisBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.monetizemaisback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonetizeMaisBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonetizeMaisBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/monetizemaisback/MonetizeMaisBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.monetizemaisback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonetizeMaisBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
